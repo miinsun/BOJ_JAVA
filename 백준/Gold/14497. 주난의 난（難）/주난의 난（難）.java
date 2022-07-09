4c629a5fc179ab27e728a8d74da1081e1178b2a9
민선 <61786235+miinsun@users.noreply.github.com>
2022-07-09 19:19:47 +0900

    [Gold IV] Title: 주난의 난(難), Time: 200 ms, Memory: 20908 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -16,13 +16,45 @@ class Point{
 public class Main {
 	static int[] dx = {-1, 0, 1, 0};
 	static int[] dy = {0, 1, 0, -1};
+	static char[][] board;
+	static int n,m;
+	
+	public static void bfs(int x, int y, int[][] checked) {
+
+		Deque<Point> q = new LinkedList<>();
+		q.add(new Point(x, y));
+		checked[x][y] = 1;
+		
+		while(!q.isEmpty()) {
+			Point cur = q.poll();
+
+			for(int i = 0; i < 4; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				
+				// 외곽선 검사
+				if(nx >= 0 && nx < n && ny >= 0 && ny < m && checked[nx][ny] == 0) {
+					if(board[nx][ny] == '1' || board[nx][ny] == '#') {
+						checked[nx][ny] = checked[cur.x][cur.y] + 1;
+						q.add(new Point(nx, ny));
+					}
+					else if(board[nx][ny] == '0') {
+						checked[nx][ny] = checked[cur.x][cur.y];
+						q.addFirst(new Point(nx, ny));
+					}
+
+					
+				}
+			}
+		}
+	}
 	
 	public static void main(String[] args)throws IOException { 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
 		
 		// 주난의 위치
@@ -33,11 +65,8 @@ public class Main {
 		int x2 = Integer.parseInt(st.nextToken()) - 1;
 		int y2 = Integer.parseInt(st.nextToken()) - 1;
 		
-		char[][] board = new char[n][m];
-		int[][] visited = new int[n][m];
-		for(int i = 0; i < n; i++) {
-			Arrays.fill(visited[i], -1);
-		}
+		// 교실 정보 입력
+		board = new char[n][m];
 		for(int i = 0; i < n; i++) {
 			String s = br.readLine();
 			for(int j = 0; j < m; j++) {
@@ -45,34 +74,13 @@ public class Main {
 			}
 		}
 		
-		Deque<Point> q = new LinkedList<>();
-		q.add(new Point(x1, y1));
-		visited[x1][y1] = 0;
+		// 주난이의 점프 결과를 저장
+		int[][] checked = new int[n][m];
 		
-		while(!q.isEmpty()) {
-			Point cur = q.poll();
-
-			for(int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				// 외곽선 검사
-				if(nx >= 0 && nx < n && ny >= 0 && ny < m && visited[nx][ny] == -1) {
-					if(board[nx][ny] == '1' || board[nx][ny] == '#') {
-						visited[nx][ny] = visited[cur.x][cur.y] + 1;
-						q.add(new Point(nx, ny));
-					}
-					else if(board[nx][ny] == '0') {
-						visited[nx][ny] = visited[cur.x][cur.y];
-						q.addFirst(new Point(nx, ny));
-					}
-
-					
-				}
-			}
-		}
+		// 주난이의 위치를 중심으로 bfs 진행
+		bfs(x1, y1, checked);
 		
-		System.out.println(visited[x2][y2]);
+		System.out.println(checked[x2][y2] - 1);
 		br.close();
 		
 	}
