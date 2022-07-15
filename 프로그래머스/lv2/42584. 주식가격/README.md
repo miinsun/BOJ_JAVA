efff85e291356c58e8b5088886867f16c088fe0c
민선 <61786235+miinsun@users.noreply.github.com>
2022-07-16 08:28:51 +0900

    [level 2] Title: 주식가격, Time: 12.76 ms, Memory: 65.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/README.md
+++ 프로그래머스/lv2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 66 MB, 시간: 13.51 ms
+메모리: 65.3 MB, 시간: 12.76 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -2,18 +2,18 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int [prices.length];
         
+        // i초의 시점을 기준으로 진행
         for(int i = 0; i < prices.length; i++){
+            // i초 이후로 검사를 진행
             for(int j = i + 1; j < prices.length; j++){
-                int cnt = 0;
-                if(prices[i] <= prices[j]){
-                    answer[i]++;
-                }
-                else{
-                    answer[i]++;
+                answer[i]++;
+
+                if(prices[i] > prices[j]){   // i초보다 작은 값이 있다면 반복문 중단
                     break;    
                 }
             }
         }
+        
         return answer;
     }
 }
