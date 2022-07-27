5e8d57d267684b7e7c8a91da121a652cdbc46646
민선 <61786235+miinsun@users.noreply.github.com>
2022-07-28 02:06:41 +0900

    [Silver II] Title: N과 M (9), Time: 796 ms, Memory: 41164 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static LinkedHashSet<String> result = new LinkedHashSet<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        visited = new boolean[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        dfs("",0);
+
+        result.forEach(System.out ::println);
+
+    }
+
+    public static void dfs(String num, int depth){
+        if(depth == M){
+            if(!result.contains(num))
+                result.add(num);
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(num + arr[i] + " ", depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
